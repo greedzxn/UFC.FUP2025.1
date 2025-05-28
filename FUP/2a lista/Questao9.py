032f598ac30cde58168982ae32306dbd805214d7
greedzxn <129681334+greedzxn@users.noreply.github.com>
2025-05-28 15:36:58 -0300

    Update Questao9.py

--- FUP/2a lista/Questao9.py
+++ FUP/2a lista/Questao9.py
@@ -1,11 +1,1 @@
-#Entrada da variáveis
-preco = float(input('Qual o valor original da compra? '))
-desconto = float(input('Qual o valor do desconto? '))
-leao =  float(input('Qual o valor do imposto? '))
 
-#Cálculo do Algoritmo
-preco_com_desconto = preco-(preco*(desconto/100))
-preco_com_imposto = preco_com_desconto+(preco_com_desconto*(leao/100))
-
-#Resultado
-print(f'O valor final da compra é R$ {preco_com_imposto:.2f}')    
